75721384ff70db203bc4fdd028f19ff3afd8a9d8
Sabrina <sabrinalaramee@sabrinas-MacBook-Pro.local>
2019-01-11 09:00:22 -0500

    commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 $(document).foundation()
+
+
+$("[data-menu-underline-from-center] a").addClass("underline-from-center");
+
+$('.input-number-increment').click(function() {
+  var $input = $(this).parents('.input-number-group').find('.input-number');
+  var val = parseInt($input.val(), 10);
+  $input.val(val + 1);
+});
+
+$('.input-number-decrement').click(function() {
+  var $input = $(this).parents('.input-number-group').find('.input-number');
+  var val = parseInt($input.val(), 10);
+  $input.val(val - 1);
+})
+
+
